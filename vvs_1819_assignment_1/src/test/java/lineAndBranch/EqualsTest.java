14befbd8f03dd82c2919e023994efb0c3bb56b83
fc46784 <fc46784@alunos.fc.ul.pt>
2019-04-14 11:16:24 +0100

    equals
    equals completo

--- vvs_1819_assignment_1/src/test/java/lineAndBranch/EqualsTest.java
+++ vvs_1819_assignment_1/src/test/java/lineAndBranch/EqualsTest.java
@@ -35,4 +35,62 @@ public class EqualsTest {
 		assertEquals(result,false,"arvores nao sao iguais");
 
 	}	
+	
+	
+	@Test
+	public void testEqualsReferences() {
+		List<Integer> lista = new ArrayList <> ();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(5);
+		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
+		assertEquals(tree.equals(tree),true,"referencias não são iguais");
+
+	}	
+	
+	@Test
+	public void testEqualsWithSameElements () {
+		List<Integer> lista = new ArrayList <> ();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(5);
+		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
+		
+		List<Integer> list2 = new ArrayList <> ();
+		list2.add(1);
+		list2.add(2);
+		list2.add(3);
+		list2.add(4);
+		list2.add(5);
+		ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(list2,5);
+		
+		boolean result = tree.equals(tree1);
+		assertEquals(result,true,"arvores nao sao iguais");
+
+	}	
+	
+	@Test
+	public void testEqualsDifferentObjects () {
+		
+		List<Integer> lista = new ArrayList <> ();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(5);
+		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
+		
+		
+		boolean result = tree.equals(lista);
+		assertEquals(result,false,"nao tem o tipo desejado");
+
+	}	
+	
+	
+	
+	
 }
