8df639f73b704e8b23f25461f9376300d2f720e7
fc46784 <fc46784@alunos.fc.ul.pt>
2019-04-25 16:06:52 +0100

    comentários do PPC
    praticamente ponto 2 feito

--- vvs_1819_assignment_1/src/test/java/graphCoverageInsert/EgePair.java
+++ vvs_1819_assignment_1/src/test/java/graphCoverageInsert/EgePair.java
@@ -21,7 +21,8 @@ public class EgePair {
 		 * */
 	
 	
-	//[1],[2],[1,2]
+	//TR(EPC)=[1],[2],[1,2]
+	//TR(PPC)=[1,2]
 	@Test
 	public void testInsertEmptyTree () {
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(2);
@@ -30,7 +31,8 @@ public class EgePair {
 		assertEquals(tree.contains(1),true,"elemento errado");
 	}
 	
-	//[5,7][3,5,7][5,7,8]
+	//TR(EPC)=[5,7][3,5,7][5,7,8]
+	//TR(PPC)=[1,3,5,7,8]
 	@Test
 	public void testInsertIsLeaf() {
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(1,2);
@@ -39,7 +41,8 @@ public class EgePair {
 		assertEquals(tree.contains(2),true,"elemento errado");
 	}
 
-	//[3][4][1,3][3,4][1,3,4]
+	//TR(EPC)=[3][4][1,3][3,4][1,3,4]
+	//TR(PPC)=[1,3,4]
 	@Test
 	public void testInsertRepetitions() {
 
@@ -50,7 +53,8 @@ public class EgePair {
 
 	}
 	
-	//[5][6][7][8][3,5][5,6][6,7][7,8][1,3,5][3,5,6][5,6,7][6,7,8]
+	//TR(EPC)=[5][6][7][8][3,5][5,6][6,7][7,8][1,3,5][3,5,6][5,6,7][6,7,8]
+	//TR(PPC)=[1,3,5,6,7,8]
 	@Test
 	public void testSwapElementsAtRoot() {
 
@@ -62,8 +66,8 @@ public class EgePair {
 	}
 	
 	
-	//[9] [10] [11] [12] [13] [7,9] [9,10] [9,11] [11,12] [12,13] [7,9,10] [7,9,11] [9,11,12]
-	//[11,12,13]
+	//TR(EPC)=[9] [10] [11] [12] [13] [7,9] [9,10] [9,11] [11,12] [12,13] [7,9,10] [7,9,11] [9,11,12] [11,12,13]
+	//TR(PPC)=[1,3,5,7,9,10],[1,3,5,7,9,11,12,13]
 	@Test
 	public void testInsertLowerElementInNonEmptyTree() {
 
@@ -78,7 +82,8 @@ public class EgePair {
 	}
 
 
-	//[15] [16] [11,15] [15,16] [9,11,12] [9,11,15] [11,15,16] [11,15,17]
+	//TR(EPC)=[15] [16] [11,15] [15,16] [9,11,12] [9,11,15] [11,15,16] [11,15,17]
+	//TR(PPC)=[1,3,5,7,9,11,15,16]
 	@Test
 	public void testNewLevel() {
 		List<Integer> lista = new ArrayList<>();
@@ -93,7 +98,8 @@ public class EgePair {
 	}
 	
 
-	//[17] [18] [19] [20] [15,17] [17,18] [18,19] [18,20] [15,16,17] [17,18,19] [17,18,20]
+	//TR(EPC)=[17] [18] [19] [20] [15,17] [17,18] [18,19] [18,20] [15,16,17] [17,18,19] [17,18,20]
+	//TR(PPC)=[1,3,5,7,9,11,15,17,18,20],[1,3,5,7,9,11,15,17,18,19]
 	@Test
 	public void testIfNodeCapacityIsFullAndElemLagerThanAll() {
 		List<Integer> list = Arrays.asList(10,20,16,17,30,40,15);
@@ -104,7 +110,8 @@ public class EgePair {
 	}	
 
 	
-	//[11,12,14]
+	//TR(EPC)=[11,12,14]
+	//TR(PPC)=[1,3,5,7,9,11,12,14]
 	@Test
 	public void testCapacityNotFull() {
 		List<Integer> list = Arrays.asList(10,17,20,21,23,25,26,40,50,35);
