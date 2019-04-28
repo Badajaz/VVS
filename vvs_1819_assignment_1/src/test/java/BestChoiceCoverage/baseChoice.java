bdbb831053826a8c3d3b90bacfa16e0fcfc74110
fc46784 <fc46784@alunos.fc.ul.pt>
2019-04-28 14:51:45 +0100

    comentários e testes equals done
    comentários e testes equals done

--- vvs_1819_assignment_1/src/test/java/BestChoiceCoverage/baseChoice.java
+++ vvs_1819_assignment_1/src/test/java/BestChoiceCoverage/baseChoice.java
@@ -1,10 +1,9 @@
 package BestChoiceCoverage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -14,198 +13,98 @@ public class baseChoice {
 
 
 
-/*
-	@Test
-	public void testEmptyTreeOne() {
-		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(0);
-		ArrayNTree<Integer>  tree1 = tree.clone();
-		assertTrue(tree.equals(tree1),"N");
-	}
-*/
 
 	/*
 	 * 
-	 * Tree 1 instersection of Tree 2(partial)
+	 * Covers BCC = [~et1,~et2,~nt2,partial]
 	 */
+
 	@Test
 	public void testPartialEquals () {
-		List<Integer> lista = new ArrayList <> ();
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
+		List<Integer> lista = Arrays.asList(1,2,3,4,5);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-
-		List<Integer> list2 = new ArrayList <> ();
-		list2.add(2);
-		list2.add(3);
-		list2.add(4);
-		list2.add(5);
-		list2.add(6);
+		List<Integer> list2 = Arrays.asList(2,3,4,5,6);
 		ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(list2,5);
-
-		boolean result = tree.equals(tree1);
-		assertEquals(result,false,"arvores nao sao iguais");
-
+		assertFalse(tree.equals(tree1),"arvores nao sao iguais");
 	}
-	
+
 	/*
 	 * 
 	 * 
-	 * Tree 1 instersection of Tree 2(full)
+	 * Covers BCC  =[~et1,~et2,~nt2,full] 
 	 * 
 	 */
+
 	@Test
 	public void testEqualsWithSameElements () {
-		List<Integer> lista = new ArrayList <> ();
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
+		List<Integer> lista =  Arrays.asList(1,2,3,4,5);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-
-		List<Integer> list2 = new ArrayList <> ();
-		list2.add(1);
-		list2.add(2);
-		list2.add(3);
-		list2.add(4);
-		list2.add(5);
+		List<Integer> list2 =  Arrays.asList(1,2,3,4,5);
 		ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(list2,5);
-
-		boolean result = tree.equals(tree1);
-		assertEquals(result,true,"arvores nao sao iguais");
-
+		assertTrue(tree.equals(tree1),"arvores nao sao iguais");
 	}	
-	
+
 	/*
 	 * 
 	 * 
-	 * Tree 1 instersection of Tree 2(empty)
+	 * Covers BCC  = [~et1,~et2,~nt2,empty]
 	 * 
 	 */
+
 	@Test
 	public void testEqualsWithDifferentIntersections () {
-		List<Integer> lista = new ArrayList <> ();
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
+		List<Integer> lista =  Arrays.asList(1,2,3,4,5);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-
-		List<Integer> list2 = new ArrayList <> ();
-		list2.add(6);
-		list2.add(7);
-		list2.add(8);
-		list2.add(9);
-		list2.add(10);
+		List<Integer> list2 =  Arrays.asList(6,7,8,9,10);
 		ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(list2,5);
-
-		boolean result = tree.equals(tree1);
-		assertEquals(result,false,"arvores nao sao iguais");
-
+		assertFalse(tree.equals(tree1),"arvores nao sao iguais");
 	}	
-	
+
 	/*
 	 * 
 	 * 
-	 * Tree 2 is null
+	 *  Covers BCC  = [~et1,~et2,nt2,empty]
 	 * 
 	 */
+
 	@Test
 	public void testTreeNull () {
-	List<Integer> lista = new ArrayList <> ();
-	lista.add(1);
-	lista.add(2);
-	lista.add(3);
-	lista.add(4);
-	lista.add(5);
-	ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-	
-	ArrayNTree<Integer> tree2 = null;
-	assertFalse(tree.equals(tree2), "nao sao iguais");
-	
+		List<Integer> lista = Arrays.asList(1,2,3,4,5);
+		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
+		ArrayNTree<Integer> tree2 = null;
+		assertFalse(tree.equals(tree2), "nao sao iguais");
 	}
 
-	
+
 	/*
 	 * 
 	 * 
-	 * Tree 2 is empty
+	 * Covers BCC = [et1,et2,nt2,empty]
 	 * 
 	 */
+
 	@Test
 	public void testTreeEmpty () {
-	List<Integer> lista = new ArrayList <> ();
-	lista.add(1);
-	lista.add(2);
-	lista.add(3);
-	lista.add(4);
-	lista.add(5);
-	ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-	
-	ArrayNTree<Integer> tree2 = new ArrayNTree<Integer>(0);
-	assertFalse(tree.equals(tree2), "nao sao iguais");
-	
+		List<Integer> lista = Arrays.asList(1,2,3,4,5);
+		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<Integer>(0);
+		assertFalse(tree.equals(tree2), "nao sao iguais");
 	}
-	
-	
+
+
 	/*
 	 * 
 	 * 
-	 * Tree 1 is empty
+	 * Covers BCC  = [et1,et2,nt2,empty]
 	 * 
 	 */
+
 	@Test
 	public void testTree1Empty () {
-	
-	ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(0);
-	
-	List<Integer> lista = new ArrayList <> ();
-	lista.add(1);
-	lista.add(2);
-	lista.add(3);
-	lista.add(4);
-	lista.add(5);
-	ArrayNTree<Integer> tree2 = new ArrayNTree<Integer>(lista,5);
-	assertFalse(tree2.equals(tree1), "nao sao iguais");
-	
+		ArrayNTree<Integer> tree1 = new ArrayNTree<Integer>(0);
+		List<Integer> lista = Arrays.asList(1,2,3,4,5);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<Integer>(lista,5);
+		assertFalse(tree2.equals(tree1), "nao sao iguais");
 	}
-	
-	
-/*
-	@Test
-	public void testEqualsReferences() {
-		List<Integer> lista = new ArrayList <> ();
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
-		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-		assertEquals(tree.equals(tree),true,"referencias não são iguais");
 
-	}	
-
-
-	@Test
-	public void testEqualsDifferentObjects () {
-
-		List<Integer> lista = new ArrayList <> ();
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
-		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,5);
-
-
-		boolean result = tree.equals(lista);
-		assertEquals(result,false,"nao tem o tipo desejado");
-
-	}*/
-
-	
 }
--- vvs_1819_assignment_1/src/test/java/graphCoverageInsert/EgePair.java
+++ vvs_1819_assignment_1/src/test/java/graphCoverageInsert/EgePair.java
@@ -13,55 +13,72 @@ import sut.ArrayNTree;
 
 public class EgePair {
 
-	
 	/*
-	//TR(EPC)=[1],[2],[1,2]
-	//TR(PPC)=[1,2]
+	 * Covers EPC = [1],[2],[1,2]
+	 * Covers PPC = [1,2]
+	 * 
+	 */
+	
 	@Test
 	public void testInsertEmptyTree () {
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(2);
 		tree.insert(1);
 		assertEquals(tree.size(),1,"tamanho errado");
-		assertEquals(tree.contains(1),true,"elemento errado");
+		assertTrue(tree.contains(1),"elemento errado");
 	}
 	
-	//TR(EPC)=[5,7][3,5,7][5,7,8]
-	//TR(PPC)=[1,3,5,7,8]
+	/*
+	 * 
+	 * Covers EPC=[5,7][3,5,7][5,7,8]
+	 * Covers PPC =[1,3,5,7,8]
+	 * 
+	 */
+
 	@Test
 	public void testInsertIsLeaf() {
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(1,2);
 		tree.insert(2);
 		assertEquals(tree.size(),2,"tamanho errado");
-		assertEquals(tree.contains(2),true,"elemento errado");
+		assertTrue(tree.contains(2),"elemento errado");
 	}
 
-	//TR(EPC)=[3][4][1,3][3,4][1,3,4]
-	//TR(PPC)=[1,3,4]
+	/*
+	 * 
+	 * Covers EPC = [3][4][1,3][3,4][1,3,4]
+	 * Covers PPC = [1,3,4]
+	 * 
+	 */
+	
 	@Test
 	public void testInsertRepetitions() {
-
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(4);
 		tree.insert(1);
 		tree.insert(1);
 		assertEquals(tree.size(),1,"tamanho errado");		
-
 	}
 	
-	//TR(EPC)=[5][6][7][8][3,5][5,6][6,7][7,8][1,3,5][3,5,6][5,6,7][6,7,8]
-	//TR(PPC)=[1,3,5,6,7,8]
+	/*
+	 * 
+	 * Covers EPC = [5][6][7][8][3,5][5,6][6,7][7,8][1,3,5][3,5,6][5,6,7][6,7,8]
+	 * Covers PPC = [1,3,5,6,7,8]
+	 * 
+	 */
+	
 	@Test
 	public void testSwapElementsAtRoot() {
-
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(1,4);
 		tree.insert(0);
 		int value = tree.toList().get(0);
 		assertEquals(value,0,"nao fez swap dos valores quando a root e maior que o elemento inserido");
-
 	}
 	
+	/*
+	 * 
+	 * 	Covers EPC = [9] [10] [11] [12] [13] [7,9] [9,10] [9,11] [11,12] [12,13] [7,9,10] [7,9,11] [9,11,12] [11,12,13]
+	 * 	Covers PPC = [1,3,5,7,9,10],[1,3,5,7,9,11,12,13]
+	 * 
+	 */
 	
-	//TR(EPC)=[9] [10] [11] [12] [13] [7,9] [9,10] [9,11] [11,12] [12,13] [7,9,10] [7,9,11] [9,11,12] [11,12,13]
-	//TR(PPC)=[1,3,5,7,9,10],[1,3,5,7,9,11,12,13]
 	@Test
 	public void testInsertLowerElementInNonEmptyTree() {
 
@@ -75,25 +92,31 @@ public class EgePair {
 
 	}
 
+	/*
+	 * 
+	 * Covers EPC = [15] [16] [11,15] [15,16] [9,11,12] [9,11,15] [11,15,16] [11,15,17]
+	 * Covers PPC = [1,3,5,7,9,11,15,16]
+	 * 
+	 * 
+	 */
 
-	//TR(EPC)=[15] [16] [11,15] [15,16] [9,11,12] [9,11,15] [11,15,16] [11,15,17]
-	//TR(PPC)=[1,3,5,7,9,11,15,16]
 	@Test
 	public void testNewLevel() {
-		List<Integer> lista = new ArrayList<>();
-		lista.add(1);
-		lista.add(10);
-		lista.add(20);
+		List<Integer> lista = Arrays.asList(1,10,20);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,3);
 		tree.insert(5);
 		int value = tree.toList().get(1);
 		assertEquals(value,5,"nao havia lugar para inserir um elemento");
-	
 	}
 	
+	/*
+	 * 
+	 * 
+	 * Covers EPC = [17] [18] [19] [20] [15,17] [17,18] [18,19] [18,20] [15,16,17] [17,18,19] [17,18,20]
+	 * Covers PPC = [1,3,5,7,9,11,15,17,18,20],[1,3,5,7,9,11,15,17,18,19]
+	 * 
+	 */
 
-	//TR(EPC)=[17] [18] [19] [20] [15,17] [17,18] [18,19] [18,20] [15,16,17] [17,18,19] [17,18,20]
-	//TR(PPC)=[1,3,5,7,9,11,15,17,18,20],[1,3,5,7,9,11,15,17,18,19]
 	@Test
 	public void testIfNodeCapacityIsFullAndElemLagerThanAll() {
 		List<Integer> list = Arrays.asList(10,20,16,17,30,40,15);
@@ -102,10 +125,15 @@ public class EgePair {
 		tree.insert(30);
 		assertEquals(tree.contains(30),true,"nao havia lugar para inserir um elemento");
 	}	
-
 	
-	//TR(EPC)=[11,12,14]
-	//TR(PPC)=[1,3,5,7,9,11,12,14]
+	/*
+	 * 
+	 * Covers EPC = [11,12,14]
+	 * Covers PPC = [1,3,5,7,9,11,12,14]
+	 * 
+	 * 
+	 */
+
 	@Test
 	public void testCapacityNotFull() {
 		List<Integer> list = Arrays.asList(10,17,20,21,23,25,26,40,50,35);
@@ -114,8 +142,12 @@ public class EgePair {
 		tree.insert(22);
 		assertEquals(tree.contains(22),true,"nao havia lugar para inserir um elemento");
 	}
-	*/
-	//TR(EPC) = [6,7,9]
+	
+	/*
+	 * 
+	 * Covers EPC = [6,7,9]
+	 * 
+	 */
 	@Test
 	public void testAtRoot ()  {
 		List<Integer> list = Arrays.asList(10,20,16,17,30,40,15);
@@ -126,13 +158,13 @@ public class EgePair {
 	}
 	
 	
-	/*
+	
 	@Test
 	public void testLastCaseInsert() {
 		List<Integer> list = Arrays.asList(10,20,30);
 		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
 		tree.insert(15);
-		assertEquals(tree.contains(15),true,"nao havia lugar para inserir um elemento");
+		assertTrue(tree.contains(15),"nao havia lugar para inserir um elemento");
 	}
 	
 	
@@ -142,7 +174,7 @@ public class EgePair {
 		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
 		tree.delete(30);
 		tree.insert(51);
-		assertEquals(tree.contains(51),true,"nao havia lugar para inserir um elemento");
+		assertTrue(tree.contains(51),"nao havia lugar para inserir um elemento");
 	}
 	
 	
@@ -150,32 +182,22 @@ public class EgePair {
 	
 	@Test
 	public void testInsertIfSpaceAvailable() {
-
-		List<Integer> lista = new ArrayList<>();
-		lista.add(1);
-		lista.add(2);
-		lista.add(4);
+		List<Integer> lista = Arrays.asList(1,2,4);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,3);
 		tree.insert(3);
 		int value = tree.toList().get(3);
 		assertEquals(value,4,"nao havia lugar para inserir um elemento");
-
 	}
 
 	@Test
 	public void testPlaceBelowLastChild() {
-		List<Integer> lista = new ArrayList<>();
-		lista.add(1);
-		lista.add(2);
-		lista.add(7);
-		lista.add(9);
+		List<Integer> lista = Arrays.asList(1,2,7,9);
 		ArrayNTree<Integer> tree = new ArrayNTree<Integer>(lista,3);
 		tree.insert(3);
 		int value = tree.toList().get(2);
 		assertEquals(value,3,"nao havia lugar para inserir um elemento");
 	}
 
-	*/
 	
 	
 	
