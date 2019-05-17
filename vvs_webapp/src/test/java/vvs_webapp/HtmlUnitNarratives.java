999759680032aad2b8ff690cc285cdee4397e305
Badajaz <32688599+Badajaz@users.noreply.github.com>
2019-05-17 17:23:42 +0100

    já conta as linhas das tabelas
    já conta as linhas das tabelas

--- vvs_webapp/src/test/java/vvs_webapp/HtmlUnitNarratives.java
+++ vvs_webapp/src/test/java/vvs_webapp/HtmlUnitNarratives.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -16,7 +17,9 @@ import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -49,10 +52,13 @@ public class HtmlUnitNarratives {
 
 	@Test
 	public void insertNewAddressForExistingCustomer() {
-		String idJoseFaria = "1";
-		String telJoseFaria = "914276732";
-		String nomeJoseFaria = "JOSE FARIA";
-		String vatJoseFaria = "197672337";
+		final String idJoseFaria = "1";
+		final String telJoseFaria = "914276732";
+		final String nomeJoseFaria = "JOSE FARIA";
+		final String vatJoseFaria = "197672337";
+		final String ruaJoseFaria = "Rua do Ouro";
+		int adressCount;
+
 
 		assertEquals("WebAppDemo Menu", page.getTitleText());
 		HtmlAnchor customerByVATLink = page.getAnchorByHref("getCustomerByVAT.html");
@@ -76,7 +82,7 @@ public class HtmlUnitNarratives {
 			System.err.println("não deu submit");
 		}
 		String textReportPage = resultado.asText();
-		System.out.println(textReportPage);
+		//System.out.println("textReportPage = "+textReportPage);
 		assertTrue(textReportPage.contains(vatJoseFaria));
 
 		HtmlPage reportPage = null;
@@ -91,10 +97,7 @@ public class HtmlUnitNarratives {
 			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
 			requestSettings.getRequestParameters().add(new NameValuePair("vat", vatJoseFaria));
 			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-			
-			
-			
-			
+
 			try {
 				reportPage = webClient.getPage(requestSettings);
 			} catch (FailingHttpStatusCodeException e) {
@@ -102,23 +105,51 @@ public class HtmlUnitNarratives {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
 		assertTrue(reportPage.asXml().contains(nomeJoseFaria));
-		
-		
-		
-		
-		
+
+		//não contem a tabela
+		if(reportPage.asXml().contains("<table")) {
+			List<HtmlElement> items = reportPage.getByXPath("//tr");
+			adressCount = items.size()-1;
+		}
+
+
+		HtmlAnchor addAddressToCustomer  = page.getAnchorByHref("addAddressToCustomer.html");
+		HtmlPage addAddressToCustomerPage = null;
+		try {
+			addAddressToCustomerPage = (HtmlPage) addAddressToCustomer.openLinkInNewWindow();
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina address inexistente " + e.getMessage());
+		}
+
+		assertEquals("Enter Address", addAddressToCustomerPage.getTitleText());
+
+		HtmlForm addCustomerForm = addAddressToCustomerPage.getForms().get(0);
+		HtmlInput vatInputAdress = addCustomerForm.getInputByName("vat");
+		vatInput.setValueAttribute(vatJoseFaria);
+		HtmlInput adressInputAdress = addCustomerForm.getInputByName("address");
+		vatInput.setValueAttribute(ruaJoseFaria);
+		HtmlInput doorInputAdress = addCustomerForm.getInputByName("door");
+		vatInput.setValueAttribute("12");
+		HtmlInput postalCodeInputAdress = addCustomerForm.getInputByName("postalCode");
+		vatInput.setValueAttribute("1600-477");
+		HtmlInput localityInputAdress = addCustomerForm.getInputByName("locality");
+		vatInput.setValueAttribute("Lisboa");
+
+		//o butão não tem name 
+		HtmlInput insert = addCustomerForm.getInputByValue("Insert");
+		HtmlPage reportPageAdress = null;
+		try {
+			reportPageAdress = insert.click();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("não deu submit");
+		}
+
+		String textReportPageAdress = reportPageAdress.asText();
+		System.out.println(textReportPageAdress);
+
 	}
-
-
-
-
-
-
-
-
-
 
 }
