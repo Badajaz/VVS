e5417162b58a1745fbb3fd94c581d1841ac0d698
PedroGasparovsky <fauchard@live.com>
2019-05-19 14:11:49 +0100

    testes de web corrigidos

--- vvs_webapp/src/test/java/vvs_webapp/HtmlUnitNarratives.java
+++ vvs_webapp/src/test/java/vvs_webapp/HtmlUnitNarratives.java
@@ -29,33 +29,23 @@ import com.gargoylesoftware.htmlunit.util.NameValuePair;
 public class HtmlUnitNarratives {
 
 	private static final String APPLICATION_URL = "http://localhost:8080/VVS_webappdemo/";
-
-
+	private static final WebClient webClient = new WebClient(BrowserVersion.getDefault());
 	private static HtmlPage page;
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		try (final WebClient webClient = new WebClient(BrowserVersion.getDefault())) { 
+		// possible configurations needed to prevent JUnit tests to fail for complex HTML pages
+		webClient.setJavaScriptTimeout(15000);
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setCssEnabled(false);
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		webClient.getOptions().setJavaScriptEnabled(true);
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
 
-			// possible configurations needed to prevent JUnit tests to fail for complex HTML pages
-			webClient.setJavaScriptTimeout(15000);
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-			webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-			webClient.getOptions().setCssEnabled(false);
-			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-			webClient.getOptions().setJavaScriptEnabled(true);
-			webClient.getOptions().setThrowExceptionOnScriptError(false);
-
-			page = webClient.getPage(APPLICATION_URL);
-			assertEquals(200, page.getWebResponse().getStatusCode()); // OK status
-		}
+		page = webClient.getPage(APPLICATION_URL);
+		assertEquals(200, page.getWebResponse().getStatusCode()); // OK status
 	}
-
-
-	/**
-	 * Caso para inserir um nova morada um utilizador já existente. 
-	 */
-
 	@Test
 	public void insertNewAddressForExistingCustomer() {
 
@@ -71,42 +61,38 @@ public class HtmlUnitNarratives {
 			System.err.println("link para a pagina errada");
 		}
 		String[] customerInfo = getFirstCustomerInfo();
-		String nomeJoseFaria = customerInfo[0];
-		String vatJoseFaria = customerInfo[2];
-		
+		String nome = customerInfo[0];
+		String vat = customerInfo[2];
+
 		assertEquals("Enter Name", customerByVATPage.getTitleText());
 		HtmlForm customerByVATForm = customerByVATPage.getForms().get(0);
 		HtmlInput vatInput = customerByVATForm.getInputByName("vat");
-		vatInput.setValueAttribute(vatJoseFaria);
-		HtmlInput submit = customerByVATForm.getInputByName("submit");
-		HtmlPage resultado = null;
+		vatInput.setValueAttribute(vat);
 
-
-		String textReportPage = customerByVATPage.asText();
-		assertTrue(textReportPage.contains(vatJoseFaria));
+		String customerByVATPageText = customerByVATPage.asText();
+		assertTrue(customerByVATPageText.contains(vat));
 
 		HtmlPage reportPage = null;
-		try(final WebClient webClient = new WebClient(BrowserVersion.getDefault())){
-			java.net.URL url = null;
-			try {
-				url = new java.net.URL(APPLICATION_URL+"GetCustomerPageController");
-			} catch (MalformedURLException e) {
-				System.err.println("Pagina inexistente " + e.getMessage());
-			}
-			WebRequest requestSettings = new WebRequest(url,HttpMethod.GET);
-			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
-			requestSettings.getRequestParameters().add(new NameValuePair("vat", vatJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-
-			try {
-				reportPage = webClient.getPage(requestSettings);
-			} catch (FailingHttpStatusCodeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		java.net.URL url = null;
+		try {
+			url = new java.net.URL(APPLICATION_URL+"GetCustomerPageController");
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina inexistente " + e.getMessage());
 		}
-		assertTrue(reportPage.asXml().contains(nomeJoseFaria));
+		WebRequest requestSettings = new WebRequest(url,HttpMethod.GET);
+		requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+		requestSettings.getRequestParameters().add(new NameValuePair("vat", vat));
+		requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
+
+		try {
+			reportPage = webClient.getPage(requestSettings);
+		} catch (FailingHttpStatusCodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, reportPage.getWebResponse().getStatusCode());
+		assertTrue(reportPage.asXml().contains(nome));
 
 		//não contem a tabela
 		if(reportPage.asXml().contains("<table")) {
@@ -124,63 +110,54 @@ public class HtmlUnitNarratives {
 
 		assertEquals("Enter Address", addAddressToCustomerPage.getTitleText());
 
-		String ruaJoseFaria = "Rua do Ouro";
-		String doorJoseFaria = "12";
-		String postalCodeJoseFaria = "1600-477";
-		String localityCodeJoseFaria = "Lisboa";
-		
+		String rua = "Rua do Ouro";
+		String door = "12";
+		String postalCode = "1600-477";
+		String localityCode = "Lisboa";
+
 		HtmlForm addCustomerForm = addAddressToCustomerPage.getForms().get(0);
 		HtmlInput vatInputAdress = addCustomerForm.getInputByName("vat");
-		vatInputAdress.setValueAttribute(vatJoseFaria);
+		vatInputAdress.setValueAttribute(vat);
 		HtmlInput adressInputAdress = addCustomerForm.getInputByName("address");
-		
-		adressInputAdress.setValueAttribute(ruaJoseFaria);
+
+		adressInputAdress.setValueAttribute(rua);
 		HtmlInput doorInputAdress = addCustomerForm.getInputByName("door");
-		doorInputAdress.setValueAttribute(doorJoseFaria);
+		doorInputAdress.setValueAttribute(door);
 		HtmlInput postalCodeInputAdress = addCustomerForm.getInputByName("postalCode");
-		postalCodeInputAdress.setValueAttribute(postalCodeJoseFaria);
+		postalCodeInputAdress.setValueAttribute(postalCode);
 		HtmlInput localityInputAdress = addCustomerForm.getInputByName("locality");
-		localityInputAdress.setValueAttribute(localityCodeJoseFaria);
+		localityInputAdress.setValueAttribute(localityCode);
 
-		//o butão não tem name 
-		HtmlInput insert = addCustomerForm.getInputByValue("Insert");
-
-
-		String reportPageAddressText = addAddressToCustomerPage.asText();
-		assertTrue(reportPageAddressText.contains(vatJoseFaria));
-		assertTrue(reportPageAddressText.contains(ruaJoseFaria));
-		assertTrue(reportPageAddressText.contains(doorJoseFaria));
-		assertTrue(reportPageAddressText.contains(postalCodeJoseFaria));
-		assertTrue(reportPageAddressText.contains(localityCodeJoseFaria));
+		String addAddressToCustomerText = addAddressToCustomerPage.asText();
+		assertTrue(addAddressToCustomerText.contains(vat));
+		assertTrue(addAddressToCustomerText.contains(rua));
+		assertTrue(addAddressToCustomerText.contains(door));
+		assertTrue(addAddressToCustomerText.contains(postalCode));
+		assertTrue(addAddressToCustomerText.contains(localityCode));
 
 		HtmlPage report = null;
-
-		try(final WebClient webClient = new WebClient(BrowserVersion.getDefault())){
-			java.net.URL url = null;
-			try {
-				url = new java.net.URL(APPLICATION_URL+"GetCustomerPageController");
-			} catch (MalformedURLException e) {
-				System.err.println("Pagina inexistente " + e.getMessage());
-			}
-			WebRequest requestSettings = new WebRequest(url,HttpMethod.POST);
-			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
-			requestSettings.getRequestParameters().add(new NameValuePair("vat", vatJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("address", ruaJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("door", doorJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("postalCode", postalCodeJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("locality", localityCodeJoseFaria));
-			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-
-			try {
-				report = webClient.getPage(requestSettings);
-			} catch (FailingHttpStatusCodeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try {
+			url = new java.net.URL(APPLICATION_URL+"GetCustomerPageController");
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina inexistente " + e.getMessage());
 		}
-		assertTrue(report.asXml().contains(nomeJoseFaria));
-
+		requestSettings = new WebRequest(url,HttpMethod.POST);
+		requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+		requestSettings.getRequestParameters().add(new NameValuePair("vat", vat));
+		requestSettings.getRequestParameters().add(new NameValuePair("address", rua));
+		requestSettings.getRequestParameters().add(new NameValuePair("door", door));
+		requestSettings.getRequestParameters().add(new NameValuePair("postalCode", postalCode));
+		requestSettings.getRequestParameters().add(new NameValuePair("locality", localityCode));
+		requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
+		try {
+			report = webClient.getPage(requestSettings);
+		} catch (FailingHttpStatusCodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, report.getWebResponse().getStatusCode());
+		assertTrue(report.asXml().contains(nome));
 
 		int adressCount2 = 0; 
 
@@ -188,16 +165,13 @@ public class HtmlUnitNarratives {
 			List<HtmlElement> items = report.getByXPath("//tr");
 			adressCount2 = items.size()-1;
 		}
-		System.out.println("adressCount = "+adressCount);
-		System.out.println("adressCount2 = "+adressCount2);
 		assertEquals(adressCount2, adressCount+1);
 	}
-
 	@Test
 	public void InsertExistingCustomerAndCheckIfReturnsError() {
 
 		assertEquals("WebAppDemo Menu", page.getTitleText());
-		
+
 		String[] customerInfo = getFirstCustomerInfo();
 		String nome = customerInfo[0];
 		String phone = customerInfo[1];
@@ -227,34 +201,29 @@ public class HtmlUnitNarratives {
 		assertTrue(insertCustomerPageText.contains(nome));
 		assertTrue(insertCustomerPageText.contains(phone));
 
-
-		HtmlInput getCustomer = insertCustomerForm.getInputByName("submit");
-
-
 		HtmlPage report = null;
-		try(final WebClient webClient = new WebClient(BrowserVersion.getDefault())){
-			java.net.URL url = null;
-			try {
-				url = new java.net.URL(APPLICATION_URL+"AddCustomerPageController");
-			} catch (MalformedURLException e) {
-				System.err.println("Pagina inexistente " + e.getMessage());
-			}
-			WebRequest requestSettings = new WebRequest(url,HttpMethod.POST);
-			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
-			requestSettings.getRequestParameters().add(new NameValuePair("vat", vat));
-			requestSettings.getRequestParameters().add(new NameValuePair("designation", nome));
-			requestSettings.getRequestParameters().add(new NameValuePair("phone", phone));
-			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-
-			try {
-				report = webClient.getPage(requestSettings);
-			} catch (FailingHttpStatusCodeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		java.net.URL url = null;
+		try {
+			url = new java.net.URL(APPLICATION_URL+"AddCustomerPageController");
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina inexistente " + e.getMessage());
 		}
-		
+		WebRequest requestSettings = new WebRequest(url,HttpMethod.POST);
+		requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+		requestSettings.getRequestParameters().add(new NameValuePair("vat", vat));
+		requestSettings.getRequestParameters().add(new NameValuePair("designation", nome));
+		requestSettings.getRequestParameters().add(new NameValuePair("phone", phone));
+		requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
+
+		try {
+			report = webClient.getPage(requestSettings);
+		} catch (FailingHttpStatusCodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, report.getWebResponse().getStatusCode());
+
 		List<Object> mensagemErro = null;
 		if(report.asXml().contains("<li>")) {
 			//obter o nome do 1º elemento
@@ -262,11 +231,7 @@ public class HtmlUnitNarratives {
 		}
 		Object mensagemErroSuposta = "It was not possible to fulfill the request: Can't add customer with vat number "+vat+".";
 		assertTrue(mensagemErro.get(0).toString().equals(mensagemErroSuposta));
-
-		//o erro vai para o controller AddCustomerPageController
-
 	}
-
 	@Test
 	public void createAndRemoveCustomer() {
 
@@ -297,44 +262,34 @@ public class HtmlUnitNarratives {
 
 		HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
 		phoneInput.setValueAttribute(phoneNewCustomer);
-
-		HtmlInput submit = addCustomerForm.getInputByName("submit");
-		HtmlPage reportPage = null;
-
-		try {
-			reportPage = submit.click();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		String textReportPage = reportPage.asText();
+		
+		String textReportPage = addCustomerPage.asText();
 		assertTrue(textReportPage.contains(vatNewCustomer));
 		assertTrue(textReportPage.contains(designationNewCustomer));
 		assertTrue(textReportPage.contains(phoneNewCustomer));
 
 		HtmlPage report = null;
-		try(final WebClient webClient = new WebClient(BrowserVersion.getDefault())){
-			java.net.URL url = null;
-			try {
-				url = new java.net.URL(APPLICATION_URL+"AddCustomerPageController");
-			} catch (MalformedURLException e) {
-				System.err.println("Pagina inexistente " + e.getMessage());
-			}
-			WebRequest requestSettings = new WebRequest(url,HttpMethod.POST);
-			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
-			requestSettings.getRequestParameters().add(new NameValuePair("vat", vatNewCustomer));
-			requestSettings.getRequestParameters().add(new NameValuePair("designation", designationNewCustomer));
-			requestSettings.getRequestParameters().add(new NameValuePair("phone", phoneNewCustomer));
-			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-
-			try {
-				report = webClient.getPage(requestSettings);
-			} catch (FailingHttpStatusCodeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		java.net.URL url = null;
+		try {
+			url = new java.net.URL(APPLICATION_URL+"AddCustomerPageController");
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina inexistente " + e.getMessage());
 		}
+		WebRequest requestSettings = new WebRequest(url,HttpMethod.POST);
+		requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+		requestSettings.getRequestParameters().add(new NameValuePair("vat", vatNewCustomer));
+		requestSettings.getRequestParameters().add(new NameValuePair("designation", designationNewCustomer));
+		requestSettings.getRequestParameters().add(new NameValuePair("phone", phoneNewCustomer));
+		requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
+
+		try {
+			report = webClient.getPage(requestSettings);
+		} catch (FailingHttpStatusCodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, report.getWebResponse().getStatusCode());
 
 		HtmlAnchor removeCustomerLink = page.getAnchorByHref("RemoveCustomerPageController");
 		HtmlPage removeCustomerPage = null;
@@ -347,37 +302,30 @@ public class HtmlUnitNarratives {
 		assertEquals("Enter VatNumber", removeCustomerPage.getTitleText());
 
 		HtmlForm removeCustomerForm = removeCustomerPage.getForms().get(0);
-
-		HtmlInput vatInputRemove = addCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(vatNewCustomer);
-
-		HtmlInput remove = addCustomerForm.getInputByName("submit");
-
-
+		HtmlInput vatInputRemove = removeCustomerForm.getInputByName("vat");
+		vatInputRemove.setValueAttribute(vatNewCustomer);
 		String textReportPageRemove = removeCustomerPage.asText();
 		assertTrue(textReportPageRemove.contains(vatNewCustomer));
 
 		HtmlPage reportRemove = null;
-		try(final WebClient webClient = new WebClient(BrowserVersion.getDefault())){
-			java.net.URL url = null;
-			try {
-				url = new java.net.URL(APPLICATION_URL+"RemoveCustomerPageController");
-			} catch (MalformedURLException e) {
-				System.err.println("Pagina inexistente " + e.getMessage());
-			}
-			WebRequest requestSettings = new WebRequest(url,HttpMethod.GET);
-			requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
-			requestSettings.getRequestParameters().add(new NameValuePair("vat", vatNewCustomer));
-			requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
-
-			try {
-				reportRemove = webClient.getPage(requestSettings);
-			} catch (FailingHttpStatusCodeException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try {
+			url = new java.net.URL(APPLICATION_URL+"RemoveCustomerPageController");
+		} catch (MalformedURLException e) {
+			System.err.println("Pagina inexistente " + e.getMessage());
 		}
+		requestSettings = new WebRequest(url,HttpMethod.GET);
+		requestSettings.setRequestParameters(new ArrayList<NameValuePair>());
+		requestSettings.getRequestParameters().add(new NameValuePair("vat", vatNewCustomer));
+		requestSettings.getRequestParameters().add(new NameValuePair("submit", "Get+Customer"));
+
+		try {
+			reportRemove = webClient.getPage(requestSettings);
+		} catch (FailingHttpStatusCodeException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, reportRemove.getWebResponse().getStatusCode());
 
 		HtmlAnchor listAllCustomersLink = page.getAnchorByHref("GetAllCustomersPageController");
 		HtmlPage listAllCustomersPage = null;
@@ -390,12 +338,9 @@ public class HtmlUnitNarratives {
 		assertEquals("Customers Info", listAllCustomersPage.getTitleText());
 
 		if (listAllCustomersPage.asXml().contains("<table")) {
-
 			assertFalse(listAllCustomersPage.asXml().contains(vatNewCustomer));
-
 		}
 	}
-
 	@Test
 	public void NewSaleForExistingCustomerWithDelivery() {
 		assertEquals("WebAppDemo Menu", page.getTitleText());
@@ -508,13 +453,13 @@ public class HtmlUnitNarratives {
 		vatInput.setValueAttribute(vat);
 
 		/**
-		 * 
-		 * se não existir a tabela nesta pagina com os address criar um address
-		 * 
-		 */
+	 * 
+	 * se não existir a tabela nesta pagina com os address criar um address
+	 * 
+	 */
 
 
-
+	
 		webClient.close();
 	}
 
@@ -552,11 +497,7 @@ public class HtmlUnitNarratives {
 			e.printStackTrace();
 		}
 		req.setRequestBody(formData);
-		WebClient webClient = null;
-
-		
 		try { 
-			webClient = new WebClient(BrowserVersion.getDefault());
 			reportPage = (HtmlPage) webClient.getPage(req);
 		} catch (FailingHttpStatusCodeException | IOException e) {
 			e.printStackTrace();
@@ -568,7 +509,6 @@ public class HtmlUnitNarratives {
 		assumeTrue(reportPage.asXml().contains("<table"));
 		List<HtmlElement> items = reportPage.getByXPath("//tr");
 
-		webClient.close();
 		return items.size()-1;
 	}
 
